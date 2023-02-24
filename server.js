46801f1a59abd29a06d6a04559e797ffe7ed471e
Hritvik Mohan <hritvik5005@gmail.com>
2023-02-24 13:28:49 +0530

    feat added cors

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express");
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+const cors = require("cors")
 
 connectDb();
 const app = express();
+app.use(cors({
+    origin: "*",
+    method: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}))
 
 const port = process.env.PORT || 5000;
 
